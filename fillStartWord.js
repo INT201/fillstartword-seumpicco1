c228115be39f47748234c04d5e8bb9570617b639
seumpicco1 <narawit.seum9@mail.kmutt.ac.th>
2022-09-04 16:47:49 +0700

    wordStart

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,19 @@
-const { template } = require('@babel/core')
+//const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  let a = startWord
+  let b = word
+if(b == null || b == undefined){
+  return undefined
+}else if (b.startsWith(a)) {
+     return b
+}else{
+     return a + b
 }
+
+}
+
 module.exports = fillStartWord
+
+//console.log(fillStartWord("1-2565-","Hello World"))
+//console.log(fillStartWord("JS","beginner"))
